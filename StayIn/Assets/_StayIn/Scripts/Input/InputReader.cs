b12df69c7b96b85a59c40df4698e2750edf9feb8
Thanh Thu <thunt14t9@gmail.com>
2026-04-09 22:22:34 +0700

    feat: complete core systems (localization & scriptableobject input)

---
+++ StayIn/Assets/_StayIn/Scripts/Input/InputReader.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+[CreateAssetMenu(fileName = "InputReader", menuName = "StayIn/Input Reader")]
+public class InputReader : ScriptableObject, StayInInput.IPlayerActions {
+
+    public event UnityAction<Vector2> MoveEvent = delegate { };
+    public event UnityAction InteractEvent = delegate { };
+    public event UnityAction InteractCanceledEvent = delegate { };
+
+    private StayInInput _gameInput;
+
+    private void OnEnable() {
+        if(_gameInput != null) {
+            _gameInput = new StayInInput();
+            _gameInput.Player.SetCallbacks(this);
+        }
+        _gameInput.Player.Enable();
+    }
+
+    private void OnDisable() {
+        _gameInput.Player.Disable();
+    }
+
+    public void OnMove(InputAction.CallbackContext context) {
+        MoveEvent.Invoke(context.ReadValue<Vector2>());
+    }
+
+    public void OnInteract(InputAction.CallbackContext context) {
+        if(context.phase == InputActionPhase.Performed) {
+            InteractEvent.Invoke();
+        } else if (context.phase == InputActionPhase.Canceled) {
+            InteractCanceledEvent.Invoke();
+        }
+    }
+
+    
+}
