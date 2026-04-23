e7fb8da32cf29d0d3fc294550f8a856d55169f73
Thanh Thu <thunt14t9@gmail.com>
2026-04-23 14:41:03 +0700

    add: logic for log manager

---
+++ StayIn/Assets/_StayIn/Scripts/Log/LogManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LogManager : MonoBehaviour
+{
+    private static LogManager instance;
+    public static LogManager Instance
+    {
+        get
+        {
+            if(instance == null)
+            {
+                instance = FindAnyObjectByType<LogManager>();
+                if(instance == null)
+                {
+                    Debug.Log("There is no LogManager in Scene.");
+                }
+            }
+            return instance;
+        }
+    }
+
+
+}
---
+++ StayIn/Assets/_StayIn/Scripts/Log/LogPageSummaryUI.cs
@@ -0,0 +1,7 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class LogPageSummaryUI : MonoBehaviour
+{
+}
